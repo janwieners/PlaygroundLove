a4f26b68b7d26cff7822e17b407653b4680159a0
janwieners <mail@jan-wieners.de>
2021-03-06 02:03:29 +0100

    Rename.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/AberJetztSchon'),
+      dir: require('path').join(__dirname, './coverage/PlaygroundLove'),
       subdir: '.',
       reporters: [
         { type: 'html' },
